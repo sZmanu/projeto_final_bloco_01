2d0bd62309420b469035d9380d43e806f2a59aaa
sZmanu <manuellaalvesdeoliveira62@gmail.com>
2025-11-13 15:39:44 -0300

    Classe controller e CRUD criados

--- Menu.ts
+++ Menu.ts
@@ -1,9 +1,25 @@
 import leia = require('readline-sync')
 import { colors } from './src/util/Colors'
+import { Cosmetico } from './src/model/Cosmetico'
+import { Produto } from './src/model/Produto'
+import { ProdutoController } from './src/controller/ProdutoController';
 
 export function main(){
 
-    let opcao: number, continua: boolean = true
+    let produtos: ProdutoController = new ProdutoController();
+
+    let opcao, id, preco, quantidade: number
+    let nome, uso, marca: string
+    let continua: boolean = true
+
+   let cosmetico1: Cosmetico = new Cosmetico(produtos.gerarNumero(), "Creme Hidratante", 35.00, 3, "Cerave", "Corporal")
+   produtos.cadastrarProduto(cosmetico1)
+    
+    let cosmetico2: Cosmetico = new Cosmetico(produtos.gerarNumero(), "Serum", 105.00, 1, "Dior", "Rosto")
+    produtos.cadastrarProduto(cosmetico2)
+
+    produtos.listarProdutos()
+   
 
     while(continua){
         console.log(colors.fg.yellowstrong, "--------------------------------------------", colors.reset)
@@ -21,16 +37,67 @@ export function main(){
 
         switch(opcao){
             case 1:
+                console.log("Digite o nome do produto: ")
+                nome = leia.question()
+
+                console.log("Digite o preço do produto: ")
+                preco = leia.questionInt()
+
+                console.log("Digite a marca do produto: ")
+                marca = leia.question()
+
+                console.log("Digite a quantidade de produto: ")
+                quantidade = leia.questionInt()
+
+                console.log("Digite a área que o produto pode ser usado: ")
+                uso = leia.question()
+
+                produtos.cadastrarProduto(new Cosmetico(produtos.gerarNumero(), nome, preco, quantidade, marca, uso))
+
                 console.log(colors.fg.greenstrong, "\nCosmetico cadastrado com sucesso!", colors.reset)
                 break
+
             case 2:
                 console.log(colors.fg.greenstrong, "\nLista de todos os cosmetico cadastrado:\n", colors.reset)
+                produtos.listarProdutos()
+
                 break
             case 3:
-                console.log(colors.fg.greenstrong, "\nCosmetico Editado com sucesso!", colors.reset)
+                console.log("Digite o número da Conta: ");
+                id = leia.questionInt()
+
+                let produto = produtos.procurarProdutoArray(id)
+
+                if (produto != null) {
+
+                console.log("Digite o nome do produto: ")
+                nome = leia.question()
+
+                console.log("Digite o preço do produto: ")
+                preco = leia.questionInt()
+
+                console.log("Digite a marca do produto: ")
+                marca = leia.question()
+
+                console.log("Digite a quantidade de produto: ")
+                quantidade = leia.questionInt()
+
+                console.log("Digite a área que o produto pode ser usado: ")
+                uso = leia.question()
+
+                produtos.atualizarProduto(new Cosmetico(id, nome, preco, quantidade, marca, uso) )
+
+
+                }else {
+                    console.log(colors.fg.red, "\nA Conta numero: " + id +
+                        " não foi encontrada!", colors.reset);
+                }
+                
                 break
             case 4:
-                console.log(colors.fg.greenstrong, "\nCosmetico excluido com sucesso!", colors.reset)
+                console.log("Digite o id do produto: ");
+                id = leia.questionInt()
+                produtos.deletarProduto(id)
                 break
             case 5:
                 console.log(colors.fg.magentastrong,"\n             Até breve, volte sempre 💕", colors.reset)
